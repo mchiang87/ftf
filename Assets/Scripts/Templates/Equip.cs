ffd5eb73f8232ef5d2875616e8a41eb8b36339b4
vaelan <mchiang87@gmail.com>
2020-07-31 10:47:47 -0500

    added base equip database

---
+++ Assets/Scripts/Templates/Equip.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Will be called solely from the party submenu and tied to item database
+public class Equip {
+    public int equipID;
+    public int itemID;
+    // public Behavior behavior; // will need to plan/create/integrate equip modifiers with player mechanics
+    // public Sprite thumbnail;
+    // public Sprite portrait;
+
+    public Equip(int equipID,int itemID) {
+        this.equipID = equipID;
+        this.itemID = itemID;
+        // this.behavior = behavior;
+        // this.thumbnail = Resource.Load<Sprite>("Sprites/Items/" + itemID);
+        // this.portrait = Resource.Load<Sprite>("Sprites/Items/" + itemID);
+    }
+}
