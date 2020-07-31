56328d3c30d6c79f67054dda6611dfbc7c7b136c
vaelan <mchiang87@gmail.com>
2020-07-31 11:14:09 -0500

    setup for game saving

--- Assets/Scripts/Templates/Quest.cs
+++ Assets/Scripts/Templates/Quest.cs
@@ -7,11 +7,13 @@ public class Quest {
     public string description;
     public List<string> objectives;
     public int rewardID;
+    public bool completed;
 
-    public Quest(int questID, string description, List<string> objectives, int rewardID) {
+    public Quest(int questID, string description, List<string> objectives, int rewardID, bool completed) {
         this.questID = questID;
         this.description = description;
         this.objectives = objectives;
         this.rewardID = rewardID;
+        this.completed = completed;
     }
 }
--- Assets/Scripts/Templates/Recipe.cs
+++ Assets/Scripts/Templates/Recipe.cs
@@ -7,14 +7,16 @@ public class Recipe {
     public string recipeName;
     public string description;
     public List<int> ingredients;
+    public bool discovered;
     // public Sprite portrait;
     // public int itemID; will be used to reference recipe's dish portrait
 
-    public Recipe(int recipeID, string recipeName, string description, List<int> ingredients) {
+    public Recipe(int recipeID, string recipeName, string description, List<int> ingredients, bool discovered) {
         this.recipeID = recipeID;
         this.recipeName = recipeName;
         this.description = description;
         this.ingredients = ingredients;
+        this.discovered = discovered;
         // this.portrait = Resource.Load<Sprite>("Portraits/Items/" + itemID);
     }
 }
