55987693c3cae098fc5d21c51e3795ea4426f52e
vaelan <mchiang87@gmail.com>
2020-08-17 13:08:41 -0500

    crafting/ui work

--- Assets/Scripts/Databases/RecipeDatabase.cs
+++ Assets/Scripts/Databases/RecipeDatabase.cs
@@ -14,33 +14,33 @@ public class RecipeDatabase : MonoBehaviour {
 
   private void BuildDatabase() {
     recipes = new List<Recipe>() {
-        new Recipe(0, "Roast", "A roast", 2.0f,
+        new Recipe(0, "Roast", "A roast", 2.0f, 1,
           new List<int>() {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 14, 15}
         ),
-        new Recipe(1, "Stew", "Stew", 1.5f,
+        new Recipe(1, "Stew", "Stew", 1.5f, 1,
           new List<int>() {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 14, 15},
           new List<int>() {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 14, 15}
         ),
-        new Recipe(1, "Fruit Tart", "Fruit Tart", 1.0f,
+        new Recipe(2, "Fruit Tart", "Fruit Tart", 1.0f, 2,
           new List<int>() {3},
           new List<int>() {16}
         ),
-        new Recipe(0, "Biscuit", "A soft biscuit", 1.0f,
+        new Recipe(3, "Biscuit", "A soft biscuit", 1.0f, 0,
           new List<int>() {16}
         ),
-        new Recipe(0, "Rice and Beans", "Rice and Beans", 3.0f,
+        new Recipe(4, "Rice and Beans", "Rice and Beans", 3.0f, 1,
           new List<int>() {4},
           new List<int>() {12}
         ),
-        new Recipe(0, "Baked tuber", "A baked tuber", 1.0f,
+        new Recipe(5, "Baked tuber", "A baked tuber", 1.0f, 0,
           new List<int>() {9}
         ),
-        new Recipe(0, "Jambalaya", "Jambalaya", 5.0f,
+        new Recipe(6, "Jambalaya", "Jambalaya", 5.0f, 1,
           new List<int>() {1, 4, 7, 9, 10, 13},
           new List<int>() {11},
           new List<int>() {21} // mirepoix
         ),
-        new Recipe(0, "Mirepoix", "Mirepoix", 1.5f,
+        new Recipe(7, "Mirepoix", "Mirepoix", 1.5f, 3,
           new List<int>() {2, 6, 14},
           new List<int>() {2, 6, 14},
           new List<int>() {20} // onion
--- Assets/Scripts/Templates/Recipe.cs
+++ Assets/Scripts/Templates/Recipe.cs
@@ -7,17 +7,19 @@ public class Recipe {
     public string name;
     public string flavorText;
     public float multiplier;
+    public int type; // 0-appetizer, 1-main, 2-dessert, 3-subingredient
     public List<int> firstSlotIngredient;
     public List<int> secondSlotIngredient;
     public List<int> thirdSlotIngredient;
     // public Sprite portrait;
     // public int itemID; will be used to reference recipe's dish portrait
 
-    public Recipe(int ID, string name, string flavorText, float multiplier, List<int> firstSlotIngredient, List<int> secondSlotIngredient = null, List<int> thirdSlotIngredient = null) {
+    public Recipe(int ID, string name, string flavorText, float multiplier, int type, List<int> firstSlotIngredient, List<int> secondSlotIngredient = null, List<int> thirdSlotIngredient = null) {
         this.ID = ID;
         this.name = name;
         this.flavorText = flavorText;
         this.multiplier = multiplier;
+        this.type = type;
         this.firstSlotIngredient = firstSlotIngredient;
         this.secondSlotIngredient = secondSlotIngredient;
         this.thirdSlotIngredient = thirdSlotIngredient;
